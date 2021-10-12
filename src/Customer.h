57407f3ea8ab9dab5cc1463928b91d5e3ce49ac6
dylanjhar <dylanjharper29@gmail.com>
2021-10-12 11:44:32 -0500

    headers written

--- src/Customer.h
+++ src/Customer.h
@@ -8,9 +8,28 @@
 #ifndef CUSTOMER_H_
 #define CUSTOMER_H_
 
+#include <string>
+using namespace std;
+
 class Customer {
+private:
+	string _fName;
+	string _lName;
+	string _userName;
+	string _password;
+	string _passReset;
+	int _credits;
+
 public:
-	Customer();
+	Customer(string first, string last, string user, string password, string passReset);
+	void setFName;
+	string getFName;
+	void setLName;
+	string getLName;
+	void setUserName;
+	string getUserName;
+	void setPassword;
+	void setPassReset;
 };
 
 #endif /* CUSTOMER_H_ */
