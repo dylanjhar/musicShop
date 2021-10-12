0e64ba38771cd6b922bf06f102be0ffda0d52bab
dylanjhar <dylanjharper29@gmail.com>
2021-10-12 12:15:53 -0500

    updated method headers

--- src/Customer.h
+++ src/Customer.h
@@ -13,23 +13,24 @@ using namespace std;
 
 class Customer {
 private:
-	string _fName;
-	string _lName;
+	string _name;
 	string _userName;
 	string _password;
 	string _passReset;
 	int _credits;
 
 public:
-	Customer(string first, string last, string user, string password, string passReset);
-	void setFName();
-	string getFName();
-	void setLName();
-	string getLName();
-	void setUserName();
+	Customer(string first, string user, string password, string passReset);
+	void setName(string name);
+	string getName();
+	void setUserName(string user);
 	string getUserName();
-	void setPassword();
-	void setPassReset();
+	void setPassword(string password);
+	string getPassword();
+	void setPassReset(string passReset);
+	string getPassReset();
+	void setCredits(int credits);
+	int getCredits();
 };
 
 #endif /* CUSTOMER_H_ */
