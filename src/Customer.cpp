ef76a97c383192cce578553fc2a25334f274f0d6
dylanjhar <dylanjharper29@gmail.com>
2021-10-12 12:16:37 -0500

    wrote method bodies

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -7,8 +7,50 @@
 
 #include "Customer.h"
 
-Customer::Customer() {
-	// TODO Auto-generated constructor stub
-
+Customer::Customer(string name, string user, string password, string passReset) {
+	_name = name;
+	_userName = user;
+	_password = password;
+	_passReset = passReset;
+	_credits = 100;
 }
 
+void Customer::setName(string first) {
+	_name = first;
+}
+
+string Customer::getName() {
+	return _name;
+}
+
+void Customer::setUserName(string user) {
+	_userName = user;
+}
+
+string Customer::getUserName() {
+	return _userName;
+}
+
+void Customer::setPassword(string password) {
+	_password = password;
+}
+
+string Customer::getPassword() {
+	return _password;
+}
+
+void Customer::setPassReset(string passReset) {
+	_passReset = passReset;
+}
+
+string Customer::getPassReset() {
+	return _passReset;
+}
+
+void Customer::setCredits(int credits) {
+	_credits = credits;
+}
+
+int Customer::getCredits() {
+	return _credits;
+}
